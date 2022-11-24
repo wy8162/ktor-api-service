c5079f32f143d8ad6511c66cda5a0a72a755a1b5
Yang Wang <wy8162@gmail.com>
2022-11-23 20:21:40 -0500

    New ApiContext

--- src/main/kotlin/com/wy8162/Application.kt
+++ src/main/kotlin/com/wy8162/Application.kt
@@ -10,44 +10,61 @@ import com.wy8162.plugins.registerKoinModules
 import com.wy8162.plugins.registerMonitoringModule
 import com.wy8162.plugins.registerSecurityModule
 import com.wy8162.plugins.registerSwaggerRoutes
+import io.ktor.client.HttpClient
+import io.ktor.server.engine.addShutdownHook
 import io.ktor.server.engine.applicationEngineEnvironment
 import io.ktor.server.engine.connector
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
+import io.ktor.server.netty.NettyApplicationEngine
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.runBlocking
+import org.koin.core.context.GlobalContext
 import java.lang.management.ManagementFactory
 import java.lang.management.RuntimeMXBean
+import java.time.Duration
 
-fun main() {
-    val env = applicationEngineEnvironment {
-        val javaAgent = getAgentArgument()
-        log.info("Application is starting in environment: ${AppConfig.applicationEnvironment()}")
-        log.info("Java agent : $javaAgent")
+fun appEnv() = applicationEngineEnvironment {
+    val javaAgent = getAgentArgument()
+    log.info("Application is starting in environment: ${AppConfig.applicationEnvironment()}")
+    log.info("Java agent : $javaAgent")
 
-        @Suppress("unused") // application.conf references the main function. This annotation prevents the IDE from marking it as unused.
-        module() {
-            registerKoinModules()
-            registerApiModule()
-            registerMonitoringModule()
-            registerApiV1Routes()
-            registerHrApiV1Routes()
-            registerSwaggerRoutes()
-            registerErrorHandlingModule()
-            initializeDatabase()
-            registerSecurityModule()
-        }
-
-        // Don't do this for production.
-        developmentMode = true
-
-        connector {
-            port = AppConfig.appServerPort()
-        }
-        connector {
-            port = AppConfig.appMetricServerPort()
-        }
+    @Suppress("unused") // application.conf references the main function. This annotation prevents the IDE from marking it as unused.
+    module() {
+        registerKoinModules()
+        registerApiModule()
+        registerMonitoringModule()
+        registerApiV1Routes()
+        registerHrApiV1Routes()
+        registerSwaggerRoutes()
+        registerErrorHandlingModule()
+        initializeDatabase()
+        registerSecurityModule()
     }
 
-    embeddedServer(Netty, environment = env).start(wait = true)
+    // Don't do this for production.
+    developmentMode = true
+
+    connector {
+        port = AppConfig.appServerPort()
+    }
+    connector {
+        port = AppConfig.appMetricServerPort()
+    }
+}
+
+fun main() {
+    val preWait: Duration = Duration.ofSeconds(2)
+    val graceShutdown: Duration = Duration.ofSeconds(2)
+    val timeout: Duration = Duration.ofSeconds(5)
+
+    val httpClient: HttpClient by lazy { GlobalContext.get().get() }
+
+    val engine = embeddedServer(Netty, environment = appEnv())
+    engine.addShutdownHook {
+        gracefulShutdownHook(engine, httpClient, preWait, graceShutdown, timeout)
+    }
+    engine.start(wait = true)
 }
 
 private fun getAgentArgument(): String? {
@@ -58,4 +75,20 @@ private fun getAgentArgument(): String? {
         }
     }
     return "Agent jar not found"
+}
+
+private fun gracefulShutdownHook(
+    engine: NettyApplicationEngine,
+    httpClient: HttpClient,
+    preWait: Duration,
+    graceShutdown: Duration,
+    timeout: Duration
+) {
+    engine.environment.log.info("Shutting down in progress...")
+
+    runBlocking {
+        delay(preWait.toMillis())
+        engine.stop(graceShutdown.toMillis(), timeout.toMillis())
+    }
+    httpClient.close()
 }
